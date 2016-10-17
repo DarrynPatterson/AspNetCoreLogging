4e75efeb5552231469bec3abc865c3fa72e50463
Darryn Patterson <darryn@whereismytransport.com>
2016-10-17 08:30:06 +0200

    Added Azure blob storage as a logging provider.

--- src/AspNetCoreLogging/LoggerFactoryExtensions.cs
+++ src/AspNetCoreLogging/LoggerFactoryExtensions.cs
@@ -1,10 +1,20 @@
-﻿using AspNetCoreLogging.Logging.DebugOutput;
+﻿using AspNetCoreLogging.Logging.AppendBlob;
+using AspNetCoreLogging.Logging.DebugOutput;
 using Microsoft.Extensions.Logging;
 
 namespace AspNetCoreLogging
 {
     public static class LoggerFactoryExtensions
     {
+        public static ILoggerFactory AddAppendBlob(
+            this ILoggerFactory factory,
+            string connectionString,
+            LogLevel minimumLevel)
+        {
+            factory.AddProvider(new AppendBlobLoggerProvider(connectionString, minimumLevel));
+            return factory;
+        }
+
         public static ILoggerFactory AddDebug(
             this ILoggerFactory factory,
             LogLevel minimumLevel)
